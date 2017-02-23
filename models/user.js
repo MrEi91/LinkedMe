eff31b1cbc2d2da3202183600a655e69453788b1
Eri Irawan <eryirawan91@gmail.com>
2017-02-23 22:23:21 +0700

    register done, tapi belum pake hased(encrypt)

--- models/user.js
+++ models/user.js
@@ -1,16 +1,71 @@
 'use strict';
+
+const crypto = require('crypto')
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING
+    email:{
+      type:DataTypes.STRING,
+      validate:{
+        isEmail:true,
+        isUnique:function(value, next){
+          User.find({
+            where:{
+              email:value
+            },attributes:["id"]
+          }).done(function(user){
+            if (user) {
+              return next(`Email address already in use`)
+            }
+            next()
+          })
+        }
+      }
+    },
+    phone:{
+      type:DataTypes.STRING,
+      validate:{
+        len:{
+          args:[10,13],
+          msg:"Phone length must be 10-13"
+        },
+        isNumeric:{
+          args:true,
+          msg:"Phone not allow letters"
+        },
+        isAlphanumeric:{
+          args:false,
+          msg:"Phone not allow alphanumeric"
+        }
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
     }
+    // hooks:{
+    //   beforeCreate:function(value, option){
+    //     let unique = "abcdefghijklmnopqrsstuvwxyz0123456789"
+    //     let salt = ''
+    //
+    //     for (let i = 0; i < 9; i++) {
+    //       salt += unique[Math.floor(Math.random() * unique.length)]
+    //     }
+    //
+    //     value.salt = salt
+    //
+    //     const hash = crypto.createHmac('sha512', salt)
+    //                        .update(value.password)
+    //                        .digest('hex')
+    //
+    //     value.password = hash
+    //
+    //   }
+    // }
   });
   return User;
 };
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,32 @@ var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index')
 });
 
-router.post('/', function(req, res){
-  models.User.create({
-    first_name : req.body.firstname,
-    last_name: req.body.lastname,
-    email: req.body.email,
-    password : req.body.password
-  }).then(function(users){
+router.get('/get', function(req, res, next) {
+  models.User.findAll().then((users) =>{
+    res.send(users)
+  })
+});
 
+router.post('/add', function(req, res){
+  models.User.findOne({where: {
+    email:req.body.email
+  }
+}).then((data) => {
+  console.log(data);
+    if(!data) {
+      models.User.create({
+        first_name : req.body.firstname,
+        last_name: req.body.lastname,
+        email: req.body.email
+      }).then(function(users){
+        res.send(users)
+      })
+    } else {
+      res.send('email geus aya')
+    }
   })
 })
 
