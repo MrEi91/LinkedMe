40c5d5c1357e40fc25f51c0b8c37012724f5be8d
Eri Irawan <eryirawan91@gmail.com>
2017-02-23 21:22:28 +0700

    modal register

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res){
+  models.User.create({
+    first_name : req.body.firstname,
+    last_name: req.body.lastname,
+    email: req.body.email,
+    password : req.body.password
+  }).then(function(users){
+
+  })
+})
 
 module.exports = router;
