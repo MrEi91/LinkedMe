1bef0bd50348f8a5142429e51da138f26f31ce2b
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-24 11:40:54 +0700

    dummy-ejs-succes-login

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,34 @@ router.get('/', function(req, res, next) {
   res.render('index')
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('dummy_ejs_for_success_login')
+});
+
 router.get('/get', function(req, res, next) {
   models.User.findAll().then((users) =>{
     res.send(users)
   })
 });
+
+router.post('/login',function(req,res,next){
+
+  models.User.findOne({
+    where: {email:req.body.email}
+  }).then((data)=>{
+
+    if (!data) {
+      res.send({data: [], success: false, message: "Login failed"})
+    }else{
+      req.session.login = true
+      res.setHeader('Content-Type', 'application/json');
+      res.send({data: data, success: true, message: "login success"})
+    }
+  })
+
+})
+
+
 
 router.post('/add', function(req, res){
   models.User.findOne({where: {
@@ -25,7 +48,7 @@ router.post('/add', function(req, res){
         email: req.body.email,
         hashPassword : req.body.password
       }).then(function(users){
-        res.send('users')
+        res.send(users)
       })
     } else {
       res.send('email geus aya')
